45cbcb5c5c0c0d7afd6a15951e8f19634dee585a
olliemil <oliverkjaran@gmail.com>
2023-12-04 12:31:55 +0000

    emplyoess.cvs updated and modle

---
+++ model/employee.py
@@ -0,0 +1,8 @@
+class employee:
+    def __init__(self, name="", ssn = "", phone = "", adress = "", email = "", home_phone = ""):
+        self.name = name
+        self.ssn = ssn
+        self.phone = phone
+        self.adress = adress
+        self.email = email
+        self.home_phone = home_phone
