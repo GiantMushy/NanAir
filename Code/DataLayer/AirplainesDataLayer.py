c44d693400c32beadfc3ebf8f648e6b4dfff8858
olliemil <oliverkjaran@gmail.com>
2023-12-05 11:52:21 +0000

    Data layer búið? ef einhver getur lesið yfir væri gott

--- Code/DataLayer/AirplainesDataLayer.py
+++ Code/DataLayer/AirplainesDataLayer.py
@@ -1,7 +1,7 @@
 import csv
 import os
 from Models.AirplaineModel import Airplaine
-
+#finna út undefined why 
 
 
 
@@ -22,10 +22,9 @@ class AirplaineData:
         self.ensure_file_exists()
 
     def ensure_file_exists(self):
-        self.create_file_if_not_exists(self.employee_filename, ['NameOfPlane', 'CurrentLocation', 'Type',
+        self.create_file_if_not_exists(self.airplaine_filename, ['NameOfPlane', 'CurrentLocation', 'Type',
                                                                 'Manufacturer', 'Capacity',])
-        self.create_file_if_not_exists(self.Airplaine_filename, [
-                                       'type'])
+        
         
 
     def create_file_if_not_exists(self, filename, fieldnames):
@@ -39,25 +38,19 @@ class AirplaineData:
         Read all Airplaines from the Airplaine CSV file and return them as a list of Airplaine objects.
         :return: List of Airplaine objects.
         """
-        Airplaine = []
+        Airplaines = []
         try:
             with open(self.airplaine_filename, mode='r', newline='', encoding='utf-8') as file:
                 reader = csv.DictReader(file)
                 for row in reader:
-                    airplaine.append(Airplaine(**row))
+                    Airplaines.append(Airplaine(**row))
         except FileNotFoundError:
             raise FileNotFoundError(
-                f"The file {self.employee_filename} does not exist.")
+                f"The file {self.airplaine_filename} does not exist.")
         except Exception as e:
             raise Exception(f"An error occurred while reading the file: {e}")
-        return airplaine
+        return Airplaines
 
-    def read_all_pilots(self):
-        """
-        Read all pilots from the Pilot CSV file and return them as a list of Pilot objects.
-        :return: List of Pilot objects.
-        """
-        
     def add_airplaine(self, airplaine):
         """
         Add a new airplaine to the CSV file.
@@ -75,16 +68,16 @@ class AirplaineData:
             raise Exception(
                 f"An error occurred while writing to the file: {e}")
 
-    def modify_airplaine_data(self, updated_employees):
+    def modify_airplaine_data(self, updated_airplaines):
         """
         Write the updated list of airplaines to the CSV file.
-        :param updated_employees: List of airplaine objects with updated information.
+        :param updated_airplaines: List of airplaine objects with updated information.
         """
         try:
             with open(self.airplaine_filename, mode='w', newline='', encoding='utf-8') as file:
-                if updated_employees:
+                if updated_airplaines:
                     writer = csv.DictWriter(
-                        file, fieldnames=updated_employees[0].__dict__.keys())
+                        file, fieldnames=updated_airplaines[0].__dict__.keys())
                     writer.writeheader()
                     for emp in updated_airplaines:
                         writer.writerow(emp.__dict__)
