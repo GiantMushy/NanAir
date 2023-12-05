a380080712669372c163bb8f9734d143c4b34f49
olliemil <oliverkjaran@gmail.com>
2023-12-05 15:59:03 +0000

    Create dict.py

---
+++ Code/DataLayer/dict.py
@@ -0,0 +1,7 @@
+#Dictionary of airplanes
+AllAirplaines = {'id' : '001', 'Name Of Airlpaine': 'Oliver', 'Current Location': 'Nuuk', 'Type': 'DHC-8-200', 'Manufacturer': 'De Havilland', 'Capacity': '37'}
+{'id' : '002', 'Name Of Airplaine': 'Matthías', 'Current Location': '> Reykjavík', 'Type': 'DHC-8-400', 'Manufacturer': 'De Havilland', 'Capacity': '76'}
+{'id' : '003', 'Name Of Airplaine': 'Þorvarður', 'Current Location': 'Kulisuuk', 'Type': 'DHC-8-200', 'Manufacturer': 'De Havilland', 'Capacity': '37'}
+{'id' : '004', 'Name Of Airplaine': 'Benjamín', 'Current Location': 'Svalbard', 'Type': 'DHC-8-400', 'Manufacturer': 'De Havilland', 'Capacity': '76'}
+{'id' : '005', 'Name Of Airplaine': 'Edda', 'Current Location': '> Nuuk', 'Type': 'DHC-8-200', 'Manufacturer': 'De Havilland', 'Capacity': '37'}
+              
