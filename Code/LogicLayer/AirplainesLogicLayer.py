c2371a5eb44341d0a6818948c358d3fcbdfd841d
olliemil <oliverkjaran@gmail.com>
2023-12-05 16:28:23 +0000

    Update AirplainesLogicLayer.py

--- Code/LogicLayer/AirplainesLogicLayer.py
+++ Code/LogicLayer/AirplainesLogicLayer.py
@@ -15,7 +15,7 @@ def add_destination(self, NameOfPlane, CurrentLocation, Type, Manufacturer, Capa
 
         if not CurrentLocation or not CurrentLocation.isalpha():
             raise ValueError("CurrentLocation must be a non-empty string of alphabetic characters")
-#Skoða types því 1-3 eru stafir 4 og 7 eru - og 5 er tala og 6-9 er tala
+
         if not Type or not Type.isalpha():
             raise ValueError("Type must be a non-empty string with alphabetic characters and numbers")
 
@@ -25,11 +25,14 @@ def add_destination(self, NameOfPlane, CurrentLocation, Type, Manufacturer, Capa
         if not Capacity.isdigit():
             raise ValueError("Capacity must be a positive integer")
 
-        # Validate that Distance, Travel_Time, and Contact_Phone_Number are digits
+        # Validate that capacity only has digits
         try:
             Capacity = int(Capacity)
         except ValueError:
-            raise ValueError("Distance, Travel Time, and Contact Phone Number must be numeric")
+            raise ValueError("Capacity must be numeric")
+        #Validate that name of plane and current location only has letters and 
+
+        #validate tha 1-3 of type is letters and 4-7 is - and 5 is a number and 6-9 is a number
 
         # Create a new Destination object
         NewAirplaine = Airplaine(
