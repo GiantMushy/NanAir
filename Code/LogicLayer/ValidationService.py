5e3f0844c211818c0915b43cce6cadf7da96cf4e
Edda Eik Vignisdóttir <144348880+edda23@users.noreply.github.com>
2023-12-06 16:29:57 +0000

    Create ValidationService.py

---
+++ Code/LogicLayer/ValidationService.py
@@ -0,0 +1,15 @@
+from LogicLayer import EmployeeManagerLogic
+
+
+class ValidationService:    
+    def get_mock_work_trips(self):        
+        """Temporary method to return detailed mock destinations."""         
+        mock_worktrips = [{'id': '001', 'destination': "{'country': 'Greenland', 'airport': 'Nuuk Airport', 'flight_time': '3h', 'distance_from_iceland': '1500km', 'contact_name': 'John Nuuk', 'emergency_phone': '765467'}", 'departure_datetime': datetime.datetime(2012, 11, 14, 14, 32), 'return_datetime': datetime.datetime(2012, 11, 14, 20, 32), 'crew_members': ''},    
+                      {'id': '002', 'destination': "{'country': 'Greenland', 'airport': 'Nuuk Airport', 'flight_time': '3h', 'distance_from_iceland': '1500km', 'contact_name': 'John Nuuk', 'emergency_phone': '765467'}", 'departure_datetime': datetime.datetime(2011, 11, 14, 14, 32), 'return_datetime': datetime.datetime(2011, 11, 14, 20, 32), 'crew_members': '001,002,003'}]  
+        return mock_worktrips
+
+    def validate_work_trip(self, trip):
+        pass
+
+    def validate_employee_availability(self, trip):
+        pass
