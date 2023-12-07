8761cd2074ca47f55f7297e4a00cb42f01ddf879
Benjamín Ragnarsson <benjarag7@outlook.com>
2023-12-07 10:44:44 +0000

    fór til baka

--- Code/LogicLayer/LogicLayerAPI.py
+++ Code/LogicLayer/LogicLayerAPI.py
@@ -74,57 +74,9 @@ class LogicLayerAPI:
         return self.employee_logic.object_list_to_dict_list(object_list)
     
 
-        ############################## DestinationManagerLogic ###############################
-    def list_all_pilots(self):
-        """
-        Returns a list of all pilots with their general employee information.
-        :return: List of dictionaries combining Pilot and Employee objects.
-        """
-        return self.employee_logic.list_all_pilots()
+    ############################## DestinationManagerLogic ###############################
 
-    def list_all_flight_attendants(self):
-        """
-        Compiles a list of all flight attendants with their general employee information.
-        :return: List of dictionaries combining FlightAttendant and Employee objects.
-        """
-        return self.employee_logic.list_all_flight_attendants()
 
-    def is_pilot(self, employee_id):
-        """Checks if an employee is a pilot.
-
-        Args:
-            employee_id (str): The ID of the employee to check.
-
-        Returns:
-            bool: True if the employee is a pilot, False otherwise.
-        """
-        return self.employee_logic.is_pilot(employee_id)
-
-    def is_flight_attendant(self, employee_id):
-        """Checks if an employee is a flight attendant.
-
-        Args:
-            employee_id (str): The ID of the employee to check.
-
-        Returns:
-            bool: True if the employee is a flight attendant, False otherwise.
-        """
-        return self.employee_logic.is_flight_attendant(employee_id)
-
-    def modify_employee(self, employee_id, **updates):
-        """
-        Modifies the attributes of an existing employee.
-        Restrictions apply to modifying ID, name, and social security number.
-
-        :param employee_id: ID of the employee to be modified.
-        :param updates: Dictionary of updates to be applied. (modify_employee(123, phone="1234567", address="New Address")
-        :raises ValueError: If trying to modify restricted fields or employee not found.
-        """
-        return self.employee_logic.modify_employee(employee_id, **updates)
-
-    def object_list_to_dict_list(self, object_list):
-        return self.employee_logic.object_list_to_dict_list(object_list)
-    
     def is_destination(self, City):
         return self.destination_logic.is_destination(City)
     
